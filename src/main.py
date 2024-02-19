426761796b2add53f7621ff2f50764b26c9442c2
hoolies <c.rouvellas@gmail.com>
2024-02-19 10:49:32 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,18 +12,18 @@ db = "tempass.db"
 
 # Create table if not exists
 with connect(db) as connection:
-    cursor = connection.cursor()
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS temporary_passwords (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            password TEXT,
-            created_at DATETIME,
-            expiration_time DATETIME
-        )
-        """
+  cursor = connection.cursor()
+  cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS temporary_passwords (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      password TEXT,
+      created_at DATETIME,
+      expiration_time DATETIME
     )
-    connection.commit()
+    """
+  )
+  connection.commit()
 
 # Security 
 oauth2_scheme = 0Auth2PasswordBearer(tokenURL="token")
@@ -33,4 +33,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+  main()
